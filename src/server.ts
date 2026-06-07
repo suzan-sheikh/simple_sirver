6d674ef54c533450f84a08761353da06eb5870fd
suzan-sheikh <mdsuzanskh@gmail.com>
2026-06-07 10:00:15 +0600

    express middleware set

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-import express from "express"
-const app = express()
-const port = 5000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
+const port = 5000;
 
-app.get('/user', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get("/user", (req: Request, res: Response) => {
+  res.status(200).json({
+    id: "1223",
+    name: "suzan",
+    email: "email@gmail.com",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
